eaa1ed1bd1f6e6c47ab46c0211e110ada88b325c
unknown <footehud@gmail.com>
2017-08-18 02:11:00 -0400

    adjusting question text

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,59 +1,80 @@
 //friends array 
 var friends = [{
-	name:"Utena",
-	photo:"http://www.cehenze.com/web2/extra/utena_asstes/utena-460x460.jpg", 
-	scores:[
-		1, 
-		1,
-		1
-		]
-	}, {
-	name: "Iggy",
-	photo:"http://geekandsundry.com/wp-content/uploads/2017/01/ignis-ffxv.jpg",
-	scores:[
+    name: "Tifa",
+    photo: "https://vignette2.wikia.nocookie.net/finalfantasy/images/3/3e/Tifa_Hot-.jpg/revision/latest?cb=20130202095819",
+    scores: [
+        1,
+        1,
+        1
+    ]
+}, {
+    name: "Noctis",
+    photo: "http://images2.wikia.nocookie.net/__cb20130206131130/finalfantasy/images/9/96/FFXIIIversus_Noctis.png",
+    scores: [
+        1,
+        2,
+        3
+    ]
 
-		1,
-		2,
-		3
-		]
-	},{
-	name: "Prompto",
-	photo:"http://vignette4.wikia.nocookie.net/finalfantasy/images/3/38/Prompto-Photographer-FFXV.png/revision/latest?cb=20170106031156",
-	scores: [
-		2,
-		3,
-		2
-		]
-	},{
+}, {
 
-	name: "Gladiolus",
-	photo:"https://vignette3.wikia.nocookie.net/finalfantasy/images/1/1b/Ignis-Gladiolus-FFXV.png/revision/latest?cb=20170104002718",
-	scores:[
-		4,
-		4,
-		4
-		]
-	},	{
-		
-	name:"Anthy",
-	photo:"https://static.comicvine.com/uploads/scale_small/5/59032/3717894-game_art_015.jpg",
-	scores:[
-		5,
-		5,
-		5
-		]
-	}, {
-	name:"Mikasa",
-	photo:"https://myanimelist.cdn-dena.com/images/characters/9/215563.jpg",
-	scores:[
-		1,
-		3,
-		5
-		]
+    name: "Yuna",
+    photo: "http://cdn.elenalinville.com/wp-content/uploads/2015/06/Yuna.jpg",
+    scores: [
+
+        3,
+        3,
+        3
+    ]
+
+
+}, {
+    name: "Iggy",
+    photo: "http://geekandsundry.com/wp-content/uploads/2017/01/ignis-ffxv.jpg",
+    scores: [
+
+        1,
+        2,
+        3
+    ]
+}, {
+    name: "Prompto",
+    photo: "http://vignette4.wikia.nocookie.net/finalfantasy/images/3/38/Prompto-Photographer-FFXV.png/revision/latest?cb=20170106031156",
+    scores: [
+        2,
+        3,
+        2
+    ]
+}, {
+
+    name: "Gladiolus",
+    photo: "https://vignette3.wikia.nocookie.net/finalfantasy/images/1/1b/Ignis-Gladiolus-FFXV.png/revision/latest?cb=20170104002718",
+    scores: [
+        4,
+        4,
+        4
+    ]
+}, {
+
+    name: "Barret",
+    photo: "http://www.finalfantasyunion.com/charimg/barret-wallace.jpg",
+    scores: [
+        5,
+        5,
+        5
+    ]
+}, {
+    name: "Mikasa",
+    photo: "https://myanimelist.cdn-dena.com/images/characters/9/215563.jpg",
+    scores: [
+        1,
+        3,
+        5
+    ]
 }];
 
 
 
 
 //sending out the friends array to other pages
-module.exports = friends; 
+module.exports = friends;
